3a3f0f186474bce56bd22dee7f5266735fa91864
roryTheGnome <s29991@pjwstk.edu.pl>
2026-02-04 14:42:02 +0100

    error handling for deploying

--- proj1/src/App.jsx
+++ proj1/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import './App.css'
 import Footer from "./Components/Footer.jsx";
 import Card from "./Components/Card/Card.jsx";
-import Title from "@/Components/Title.jsx";
+import Title from "./Components/Title.jsx";
 
 
 function App() {
