612e08a78096a77aed78b068b96fd9375612a921
roryTheGnome <s29991@pjwstk.edu.pl>
2026-02-04 14:58:48 +0100

    error handling for deploying

--- proj1/src/App.jsx
+++ proj1/src/App.jsx
@@ -10,23 +10,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-        <div className="header">
-            <Title s="TASKS"/>
-        </div>
-
-        <div className="tasks">
-            <Card name="Task-1" description="HTML-CSS-Git" isActive={true} submitted={true} deadline={new Date().getDate()}/>
-            <Card name="Task-2" description="JS 1" isActive={true} submitted={true} deadline={new Date().getDate()}/>
-            <Card name="Task-3" description="JS 2" isActive={true} submitted={true} deadline={new Date().getDate()}/>
-            <Card name="Task-4" description="React" isActive={true} submitted={false} deadline={new Date().getDate()}/>
-            <Card name="Task-5" description="React Practice" isActive={false} submitted={false} deadline={new Date().getDate()}/>
-            <Card name="Task-6" description="TypeScript" isActive={false} submitted={false} deadline={new Date().getDate()}/>
-            <Card name="Task-7" description="TS Practice" isActive={false} submitted={false} deadline={new Date().getDate()}/>
-            <Card name="Task-8" description="Group Project" isActive={false} submitted={false} deadline={new Date().getDate()}/>
-        </div>
-        <Footer link={"https://github.com/roryTheGnome"} owner={"Gnome"} />
-    </>
+      <h1>Heelo</h1>
   )
 }
 
